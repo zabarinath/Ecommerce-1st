87ac13c2c38a41b149b041cdd1f659d28f9ecc7d
Sabarinath <007sabarinathz@gmail.com>
2023-03-28 01:49:36 +0530

    new project has created

---
+++ ec/app/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+STATE_CHOICES = (
+('Kerala','Kerala'),
+('Assam','Assam'),
+('Rajasthan','Rajasthan'),
+('Tamil nadu','Tamil nadu'),
+('Sikkim','Sikkim'),
+('Punjab','Punjab'),
+('West Bengal','West Bengal'),
+
+)
+
+
+CATEGORY_CHOICES=(
+    ('CR', 'Curd'),
+    ('ML', 'Milk'),
+    ('LS', 'Lassi'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC','Ice-Creams'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    Product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
---
+++ ec/app/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render,redirect
+from django.views import View
+from . models import Product, Cart, Customer
+from django.db.models import Count
+from . forms import CustomerRegistrationForm, CustomerProfileForm,Customer
+from django.contrib import messages
+
+
+# Create your views here.
+def home(request):
+    return render(request,"app/home.html")
+
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html",locals())
+    
+class CategoryTitle(View):
+    def get(self,request,val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request,"app/category.html",locals())
+
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request,"app/productdetail.html",locals())
+
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html',locals())
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Congratulations! User Register Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return render(request, 'app/customerregistration.html',locals())
+class ProfileView(View):
+    def get(self,request):
+        form = CustomerProfileForm()
+        return render(request,'app/profile.html',locals())
+    def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user,name=name,locality=locality,mobile=mobile,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,"Congratulations! Profile Save Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return render(request,'app/profile.html',locals())
+
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html',locals())
+
+class updateAddress(View):
+    def get(self,request,pk):
+        form = CustomerProfileForm()
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request, 'app/updateAddress.html',locals())
+    def post(self,request,pk):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request,"Congratulations! Profile update successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        
+        return redirect("address")
+def add_to_cart(request):
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect("/cart")
+
+def show_cart(request):
+    user=request.user
+    cart=Cart.objects.filter(user=user)
+    return render(request, 'app/addtocart.html',locals())
